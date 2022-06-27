b9316928ef89c25db22033b018eb3c736589d90c
bangelev <bangelevski83@gmail.com>
2022-06-27 10:52:02 +0200

    Before BUILD and push for deployment

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,9 +2,15 @@ const mongoose = require('mongoose')
 
 // const dbLocalURI = process.env.DB_LOCAL_URI
 const connectMongoDB = () => {
-        mongoose.connect(process.env.DB_LOCAL_URI).then((con) => {
-            console.log(`Connected to MongoDB ${con.connection.host}`)
-        })
+        if (process.env.NODE_ENV === 'PRODUCTION') {
+            mongoose.connect(process.env.DB_URI).then((con) => {
+                console.log(`Connected to MongoDB ${con.connection.host}`)
+            })
+        } else {
+            mongoose.connect(process.env.DB_LOCAL_URI).then((con) => {
+                console.log(`Connected to local MongoDB ${con.connection.host}`)
+            })
+        }
     }
     //TODO: should probably reset
 module.exports = connectMongoDB
--- backend/seedDB/seeder.js
+++ backend/seedDB/seeder.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose')
 const connectMongoDB = require('../config/database')
 const Product = require('../models/product')
+
 const products = require('./productSeeds.json')
+
 const path = require('path')
 const dotenv = require('dotenv')
 
 dotenv.config({ path: 'backend/config/.env' })
-const dbLocalURI = process.env.DB_LOCAL_URI
-console.log(process.env.DB_LOCAL_URI)
+const dbLocalURI = process.env.DB_URI
+
 connectMongoDB()
 
 const seedingMongoDB = async(data) => {
